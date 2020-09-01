427d8d3ae6ba0c111c5d770050b760ecd12d96d4
Lee Castelani <notayogilee@gmail.com>
2020-09-01 15:43:36 -0400

    add comments on posts

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -2,24 +2,29 @@ import axios from 'axios';
 import { setAlert } from './alert';
 import {
   GET_POSTS,
+  GET_POST,
   POST_ERROR,
-  UPDATE_LIKES
+  UPDATE_LIKES,
+  DELETE_POST,
+  ADD_POST,
+  ADD_COMMENT,
+  REMOVE_COMMENT
 } from '../actions/types';
 
 // Get posts
 export const getPosts = () => async dispatch => {
   try {
-    const res = await axios.get('./api/posts');
+    const res = await axios.get('/api/posts');
 
     dispatch({
       type: GET_POSTS,
       payload: res.data
-    })
+    });
   } catch (err) {
     dispatch({
       type: POST_ERROR,
       payload: { msg: err.message, status: err.status }
-    })
+    });
   }
 };
 
@@ -27,17 +32,17 @@ export const getPosts = () => async dispatch => {
 
 export const addLike = id => async dispatch => {
   try {
-    const res = await axios.put(`./api/posts/like/${id}`);
+    const res = await axios.put(`/api/posts/like/${id}`);
 
     dispatch({
       type: UPDATE_LIKES,
       payload: { id, likes: res.data }
-    })
+    });
   } catch (err) {
     dispatch({
       type: POST_ERROR,
       payload: { msg: err.message, status: err.status }
-    })
+    });
   }
 };
 
@@ -45,16 +50,129 @@ export const addLike = id => async dispatch => {
 
 export const removeLike = id => async dispatch => {
   try {
-    const res = await axios.put(`./api/posts/unlike/${id}`);
+    const res = await axios.put(`/api/posts/unlike/${id}`);
 
     dispatch({
       type: UPDATE_LIKES,
       payload: { id, likes: res.data }
-    })
+    });
   } catch (err) {
     dispatch({
       type: POST_ERROR,
       payload: { msg: err.message, status: err.status }
-    })
+    });
+  }
+};
+
+// Delete post
+
+export const deletePost = id => async dispatch => {
+  try {
+    await axios.delete(`/api/posts/${id}`);
+
+    dispatch({
+      type: DELETE_POST,
+      payload: id
+    });
+
+    dispatch(setAlert('Post Removed', 'success'));
+  } catch (err) {
+    dispatch({
+      type: POST_ERROR,
+      payload: { msg: err.message, status: err.status }
+    });
+  }
+};
+
+// Add post
+
+export const addPost = formData => async dispatch => {
+
+  const config = {
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  }
+  try {
+    const res = await axios.post('/api/posts', formData, config);
+
+    dispatch({
+      type: ADD_POST,
+      payload: res.data
+    });
+
+    dispatch(setAlert('Post Created', 'success'));
+  } catch (err) {
+    dispatch({
+      type: POST_ERROR,
+      payload: { msg: err.message, status: err.status }
+    });
+  }
+};
+
+// Get post
+export const getPost = id => async dispatch => {
+  try {
+    const res = await axios.get(`/api/posts/${id}`);
+
+    dispatch({
+      type: GET_POST,
+      payload: res.data
+    });
+  } catch (err) {
+    dispatch({
+      type: POST_ERROR,
+      payload: { msg: err.message, status: err.status }
+    });
+  }
+};
+
+// Add comment
+
+export const addComment = (postId, formData) => async dispatch => {
+
+  console.log(postId)
+
+  const config = {
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  }
+
+  try {
+    const res = await axios.post(`/api/posts/comment/${postId}`, formData, config);
+
+    dispatch({
+      type: ADD_COMMENT,
+      payload: res.data
+    });
+
+    dispatch(setAlert('Comment Added', 'success'));
+  } catch (err) {
+    dispatch({
+      type: POST_ERROR,
+      payload: { msg: err.message, status: err.status }
+    });
+  }
+};
+
+// Delete comment
+
+export const deleteComment = (postId, commentId) => async dispatch => {
+
+  try {
+    await axios.delete(`/api/posts/comment/${postId}/${commentId}`);
+
+    dispatch({
+      type: REMOVE_COMMENT,
+      payload: commentId
+    });
+
+    dispatch(setAlert('Comment Removed', 'success'));
+  } catch (err) {
+    dispatch({
+      type: POST_ERROR,
+      payload: { msg: err.message, status: err.status }
+    });
   }
 };
